374dd7f2ed0c5d3bee7c31a5e9d28f6c97010b3a
Felicia Leaton <fglfz8@umsystem.edu>
2021-03-12 19:46:20 -0600

    getting porgram to work

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,4 +2,14 @@
 # But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
 # For numbers which are multiples of both three and five print “FizzBuzz”.
 
+x = range(1,101)
 
+for n in x:
+    if n%3 == 0 and n%5 == 0:
+        print("Fizzbuzz")
+    elif n%3==0:
+        print("fizz")
+    elif n%5 == 0:
+        print("buzz")
+    else:
+        print(n)
