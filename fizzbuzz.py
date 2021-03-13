26725678821cb40c0209d1ee856e0b96c26acde4
Felicia Leaton <fglfz8@umsystem.edu>
2021-03-12 19:49:57 -0600

    improved the efficiency

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -3,3 +3,14 @@
 # For numbers which are multiples of both three and five print “FizzBuzz”.
 
 
+
+
+for n in range(1,101):
+    if n%3 == 0 and n%5 == 0:
+        print("Fizzbuzz")
+    elif n%3==0:
+        print("fizz")
+    elif n%5 == 0:
+        print("buzz")
+    else:
+        print(n)
